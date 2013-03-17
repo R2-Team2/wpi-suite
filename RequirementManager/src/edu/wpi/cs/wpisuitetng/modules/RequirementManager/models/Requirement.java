0f2621ad50fdc0b16362d71bb3c655526ea92184
David Mihal <eastsideski@gmail.com>
2013-03-17 15:50:32 -0400

    Create package and Requirement class

---
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementManager/models/Requirement.java
@@ -0,0 +1,31 @@
+package edu.wpi.cs.wpisuitetng.modules.RequirementManager.models;
+
+import edu.wpi.cs.wpisuitetng.modules.AbstractModel;
+
+public class Requirement extends AbstractModel {
+
+	@Override
+	public void save() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String toJSON() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean identify(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
