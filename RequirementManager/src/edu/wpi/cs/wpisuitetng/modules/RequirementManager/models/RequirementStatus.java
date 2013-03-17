6dbbc7c9f511fa1d27c79afbdb458f9a42e06ff2
David Mihal <eastsideski@gmail.com>
2013-03-17 16:17:37 -0400

    Added simple fields & Created RequirementStatus enum

---
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementManager/models/RequirementStatus.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package edu.wpi.cs.wpisuitetng.modules.RequirementManager.models;
+
+/**
+ * @author david
+ *
+ */
+public enum RequirementStatus {
+	//TODO: Add actual statuses
+	DUMMY
+}
