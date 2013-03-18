e34642aab4a9a56c662d2d8f4a78695d6df72854
djkirby <djkirby@wpi.edu>
2013-03-18 19:24:58 -0400

    Added the basis for JUnit tests (for real this time)

---
+++ RequirementManager/test/edu/wpi/cs/wpisuitetng/modules/RequirementManager/models/RequirementManagerTest.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package edu.wpi.cs.wpisuitetng.modules.RequirementManager.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Dylan
+ *
+ */
+public class RequirementManagerTest {
+
+
+}
