a93b5051909b0ab59f43711cf863b5a228d3f655
tchoopojcharoen <tchoopojcharoen.wpi.edu>
2013-03-19 10:22:16 +0700

    THe following commit consists of GUI for new requirement editor.

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementManager/RequirementManager.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementManager/RequirementManager.java
@@ -8,6 +8,7 @@ import javax.swing.ImageIcon;
 import edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule;
 import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
 
+import edu.wpi.cs.wpisuitetng.modules.RequirementManager.view.NewRequirementPanel;
 import edu.wpi.cs.wpisuitetng.modules.RequirementManager.view.ToolbarView;
 import edu.wpi.cs.wpisuitetng.modules.RequirementManager.view.MainView;
 
@@ -22,9 +23,10 @@ public class RequirementManager implements IJanewayModule {
 
 		ToolbarView toolBar = new ToolbarView();
 		MainView mainPanel = new MainView();
-
+		NewRequirementPanel newReqPanel= new NewRequirementPanel();
+		
 		// Create a tab model that contains the toolbar panel and the main content panel
-		JanewayTabModel tab1 = new JanewayTabModel(getName(), new ImageIcon(), toolBar, mainPanel);
+		JanewayTabModel tab1 = new JanewayTabModel(getName(), new ImageIcon(), toolBar, newReqPanel);
 
 		// Add the tab to the list of tabs owned by this module
 		tabs.add(tab1);
