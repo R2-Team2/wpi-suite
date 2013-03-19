814a2bf9b98ee59e670ce5099c710e3b99ebbf26
Christopher Botaish <chris@cb-mac.wifi.wpi.edu>
2013-03-19 17:18:38 -0400

    Added documentation changed

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementManager/view/ViewEventController.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementManager/view/ViewEventController.java
@@ -7,10 +7,17 @@ public class ViewEventController {
 	private MainView main = null;
 	private ToolbarView tool = null;
 
+	/**
+	 * Default constructor for ViewEventController.  Is protected to prevent instantiation.
+	 */
 	protected ViewEventController() {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Returns the singleton instance of the vieweventcontroller.
+	 * @return The instance of this controller.
+	 */
 	public static ViewEventController getInstance() {
 		if (instance == null) {
 			instance = new ViewEventController();
@@ -18,18 +25,30 @@ public class ViewEventController {
 		return instance;
 	}
 
+	/**
+	 * Sets the main view to the given view.
+	 * @param main2 the main view to be set as active.
+	 */
 	public void setMainView(MainView main2) {
 		main = main2;
 	}
 
+	/**
+	 * 
+	 * Sets the toolbarview to the given toolbar
+	 * @param tool2 the toolbar to be set as active.
+	 */
 	public void setToolBar(ToolbarView tool2) {
 		tool = tool2;
 	}
 
+	/**
+	 * Opens a new tab for the creation of a requirement.
+	 */
 	public void createRequirement() {
 		NewRequirementPanel newReq = new NewRequirementPanel();
 		main.addTab("Create Requirement", newReq);
-		main.invalidate();
+		main.invalidate(); //force the tabbedpane to redraw.
 		main.repaint();
 	}
 }
