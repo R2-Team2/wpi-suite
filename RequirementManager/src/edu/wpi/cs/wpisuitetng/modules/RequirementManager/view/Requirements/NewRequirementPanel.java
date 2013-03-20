ec858952442965810f47fd3bf3d4e72d2e413157
Christopher Botaish <chris@cb-mac.wifi.wpi.edu>
2013-03-19 22:12:41 -0400

    Refactored the NewRequirementPanel

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementManager/view/Requirements/NewRequirementPanel.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementManager/view/Requirements/NewRequirementPanel.java
@@ -15,109 +15,118 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
-public class NewRequirementPanel extends JPanel {
+public class NewRequirementPanel extends JPanel 
+{
 
+	private JPanel leftPanel;
+	private JTextField boxName;
+	private JTextArea boxDescription;
+	private JTextArea resultField;
+	
+	private JPanel rightPanel;
+	private JTextField boxReleaseNum;
+	private JComboBox dropdownStatus;
+	private JRadioButton priorityHigh;
+	private JRadioButton priorityMedium;
+	private JRadioButton priorityLow;
+	
+	/**
+	 * Default constructor.
+	 */
 	public NewRequirementPanel() {
 
 		setLayout(new GridLayout(1, 2));
 
-		JPanel leftPanel = new JPanel();
 
+		this.add(buildLeftPanel()); //add left panel
+		this.add(buildRightPanel()); //add right panel
+	}
+	
+	
+	/**
+	 * Builds the left panel.
+	 * @return the newly created and formatted left panel.
+	 */
+	private JPanel buildLeftPanel()
+	{
+		leftPanel = new JPanel();
 		JLabel labelName = new JLabel("Name");
 		JLabel labelDescription = new JLabel("Description");
-
-		final JTextField boxName = new JTextField();
-		final JTextArea boxDescription = new JTextArea();
+		
+		boxName = new JTextField();
+		boxDescription = new JTextArea();
 		boxDescription.setLineWrap(true);
 
 		boxName.setPreferredSize(new Dimension(200, 20));
 		boxDescription.setPreferredSize(new Dimension(200, 100));
 
-		final JTextArea testField = new JTextArea(); // For testing purpose
-		testField.setPreferredSize(new Dimension(200, 200));
+		resultField = new JTextArea(); // For testing purpose
+		resultField.setPreferredSize(new Dimension(200, 200));
 
-		SpringLayout layout = new SpringLayout();
+		SpringLayout leftLayout = new SpringLayout();
 
-		// Setting layout (set-> add -> adjust)
-		leftPanel.setLayout(layout);
+		leftPanel.setLayout(leftLayout);
 
+		leftLayout.putConstraint(SpringLayout.NORTH, labelName, 15,
+				SpringLayout.NORTH, leftPanel);
+		leftLayout.putConstraint(SpringLayout.WEST, labelName, 15,
+				SpringLayout.WEST, leftPanel);
+
+		leftLayout.putConstraint(SpringLayout.NORTH, boxName, 15,
+				SpringLayout.SOUTH, labelName);
+		leftLayout.putConstraint(SpringLayout.WEST, boxName, 15, SpringLayout.WEST,
+				leftPanel);
+
+		leftLayout.putConstraint(SpringLayout.NORTH, labelDescription, 15,
+				SpringLayout.SOUTH, boxName);
+		leftLayout.putConstraint(SpringLayout.WEST, labelDescription, 15,
+				SpringLayout.WEST, leftPanel);
+
+		leftLayout.putConstraint(SpringLayout.NORTH, boxDescription, 15,
+				SpringLayout.SOUTH, labelDescription);
+		leftLayout.putConstraint(SpringLayout.WEST, boxDescription, 15,
+				SpringLayout.WEST, leftPanel);
+
+		leftLayout.putConstraint(SpringLayout.NORTH, resultField, 15,
+				SpringLayout.SOUTH, boxDescription);
+		leftLayout.putConstraint(SpringLayout.WEST, resultField, 15,
+				SpringLayout.WEST, leftPanel);
+		
 		leftPanel.add(labelName);
 		leftPanel.add(boxName);
 		leftPanel.add(labelDescription);
 		leftPanel.add(boxDescription);
-		/**/leftPanel.add(testField);
-
-		layout.putConstraint(SpringLayout.NORTH, labelName, 15,
-				SpringLayout.NORTH, leftPanel);
-		layout.putConstraint(SpringLayout.WEST, labelName, 15,
-				SpringLayout.WEST, leftPanel);
-
-		layout.putConstraint(SpringLayout.NORTH, boxName, 15,
-				SpringLayout.SOUTH, labelName);
-		layout.putConstraint(SpringLayout.WEST, boxName, 15, SpringLayout.WEST,
-				leftPanel);
-
-		layout.putConstraint(SpringLayout.NORTH, labelDescription, 15,
-				SpringLayout.SOUTH, boxName);
-		layout.putConstraint(SpringLayout.WEST, labelDescription, 15,
-				SpringLayout.WEST, leftPanel);
-
-		layout.putConstraint(SpringLayout.NORTH, boxDescription, 15,
-				SpringLayout.SOUTH, labelDescription);
-		layout.putConstraint(SpringLayout.WEST, boxDescription, 15,
-				SpringLayout.WEST, leftPanel);
-
-		layout.putConstraint(SpringLayout.NORTH, testField, 15,
-				SpringLayout.SOUTH, boxDescription);
-		layout.putConstraint(SpringLayout.WEST, testField, 15,
-				SpringLayout.WEST, leftPanel);
-
-		// RIGHT Panel
-
-		JPanel rightPanel = new JPanel();
+		leftPanel.add(resultField);
+		
+		return leftPanel;
+	}
+	
+	/**
+	 * Builds the right panel
+	 * @return the newly created and formatted right panel.
+	 */
+	private JPanel buildRightPanel()
+	{
+		rightPanel = new JPanel();
 
 		JLabel labelStatus = new JLabel("Status");
 		JLabel labelPriority = new JLabel("Priority");
 		JLabel labelReleaseNum = new JLabel("Release #");
 
-		final JTextField boxReleaseNum = new JTextField();
+		boxReleaseNum = new JTextField();
 		boxReleaseNum.setPreferredSize(new Dimension(200, 20));
 
-		String[] statusString = { "Not Selected", "New", "In Progress", "Open",
-				"Complete", "Delete" };
-		final JComboBox dropdownStatus = new JComboBox(statusString);
+		String[] statusString = { "Not Selected", "New", "In Progress", "Open", "Complete", "Delete" }; //TODO change to enum
+		dropdownStatus = new JComboBox(statusString);
 
 		JPanel priorityPanel = new JPanel();
 
 		// Radio buttons
 
-		final JRadioButton priorityHigh = new JRadioButton("High");
-		final JRadioButton priorityMedium = new JRadioButton("Medium");
-		final JRadioButton priorityLow = new JRadioButton("Low");
-
-		// Toggle for Radio Button
-		// TODO: Clean this up
-		priorityHigh.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				priorityMedium.setSelected(false);
-				priorityLow.setSelected(false);
-			}
-		});
-
-		priorityMedium.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				priorityHigh.setSelected(false);
-				priorityLow.setSelected(false);
-			}
-		});
-
-		priorityLow.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				priorityMedium.setSelected(false);
-				priorityHigh.setSelected(false);
-			}
-		});
-
+		priorityHigh = new JRadioButton("High");
+		priorityMedium = new JRadioButton("Medium");
+		priorityLow = new JRadioButton("Low");
+		
 		ButtonGroup group = new ButtonGroup();
 		group.add(priorityHigh);
 		group.add(priorityMedium);
@@ -127,6 +136,7 @@ public class NewRequirementPanel extends JPanel {
 		priorityPanel.add(priorityMedium);
 		priorityPanel.add(priorityHigh);
 
+		//setup the buttons
 		JPanel buttonPanel = new JPanel();
 		JButton buttonUpdate = new JButton("Update");
 		JButton buttonCancel = new JButton("Cancel");
@@ -134,53 +144,19 @@ public class NewRequirementPanel extends JPanel {
 
 		buttonUpdate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String stringName = boxName.getText();
-				String stringDescription = boxDescription.getText();
-				String stringStatus = (String) dropdownStatus.getSelectedItem();
-				String stringPriority;
-
-				boolean stateHigh = priorityHigh.isSelected();
-				boolean stateMedium = priorityMedium.isSelected();
-				boolean stateLow = priorityLow.isSelected();
-
-				if (stateHigh)
-					stringPriority = "High";
-				else if (stateMedium)
-					stringPriority = "Medium";
-				else if (stateLow)
-					stringPriority = "Low";
-				else
-					stringPriority = "Not selected";
-
-				String stringReleaseNum = boxReleaseNum.getText();
-
-				String entireData = stringName + "\n";
-				entireData = entireData + stringDescription + "\n";
-				entireData = entireData + stringStatus + "\n";
-				entireData = entireData + stringPriority + "\n";
-				entireData = entireData + stringReleaseNum + "\n";
-
-				testField.setText(entireData);
+				update();
 			}
 		});
 
 		buttonCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				boxName.setText(null);
-				boxDescription.setText(null);
-				dropdownStatus.setSelectedItem("Not Selected");
-				priorityLow.setSelected(false);
-				priorityMedium.setSelected(false);
-				priorityHigh.setSelected(false);
-				repaint();
-				boxReleaseNum.setText(null);
-				testField.setText(null);
+				cancel();
 			}
 		});
 
 		buttonDelete.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				testField.setText("Delete");
+				delete();
 			}
 		});
 
@@ -191,15 +167,8 @@ public class NewRequirementPanel extends JPanel {
 		SpringLayout rightLayout = new SpringLayout();
 
 		rightPanel.setLayout(rightLayout);
-
-		rightPanel.add(labelStatus);
-		rightPanel.add(dropdownStatus);
-		rightPanel.add(labelPriority);
-		rightPanel.add(priorityPanel);
-		rightPanel.add(labelReleaseNum);
-		rightPanel.add(boxReleaseNum);
-		rightPanel.add(buttonPanel);
-
+ 
+		//setup the constraints for the layout.
 		rightLayout.putConstraint(SpringLayout.NORTH, labelStatus, 15,
 				SpringLayout.NORTH, rightPanel);
 		rightLayout.putConstraint(SpringLayout.WEST, labelStatus, 15,
@@ -234,9 +203,74 @@ public class NewRequirementPanel extends JPanel {
 				SpringLayout.SOUTH, boxReleaseNum);
 		rightLayout.putConstraint(SpringLayout.WEST, buttonPanel, 15,
 				SpringLayout.WEST, rightPanel);
-
-		add(leftPanel);
-		add(rightPanel);
+		
+		rightPanel.add(labelStatus);
+		rightPanel.add(dropdownStatus);
+		rightPanel.add(labelPriority);
+		rightPanel.add(priorityPanel);
+		rightPanel.add(labelReleaseNum);
+		rightPanel.add(boxReleaseNum);
+		rightPanel.add(buttonPanel);
+		
+		return rightPanel;
 	}
 
+	/**
+	 * Updates the requirement based on the inputted information.
+	 */
+	public void update()
+	{
+		String stringName = boxName.getText();
+		String stringDescription = boxDescription.getText();
+		String stringStatus = (String) dropdownStatus.getSelectedItem();
+		String stringPriority;
+
+		boolean stateHigh = priorityHigh.isSelected();
+		boolean stateMedium = priorityMedium.isSelected();
+		boolean stateLow = priorityLow.isSelected();
+
+		if (stateHigh)
+			stringPriority = "High";
+		else if (stateMedium)
+			stringPriority = "Medium";
+		else if (stateLow)
+			stringPriority = "Low";
+		else
+			stringPriority = "Not selected";
+
+		String stringReleaseNum = boxReleaseNum.getText();
+
+		String entireData = stringName + "\n";
+		entireData = entireData + stringDescription + "\n";
+		entireData = entireData + stringStatus + "\n";
+		entireData = entireData + stringPriority + "\n";
+		entireData = entireData + stringReleaseNum + "\n";
+
+		resultField.setText(entireData);
+	}
+	
+	/**
+	 * Deletes the requirement.
+	 */
+	public void delete()
+	{
+		resultField.setText("Delete");
+	}
+	
+	/**
+	 * Cancels the editing of the requirement.
+	 */
+	public void cancel() 
+	{
+		boxName.setText("");
+		boxDescription.setText("");
+		dropdownStatus.setSelectedItem("Not Selected");
+		priorityLow.setSelected(false);
+		priorityMedium.setSelected(false);
+		priorityHigh.setSelected(false);
+		boxReleaseNum.setText("");
+		resultField.setText("");
+		repaint(); //repaint the entire panel.
+	}
+ 
 }
