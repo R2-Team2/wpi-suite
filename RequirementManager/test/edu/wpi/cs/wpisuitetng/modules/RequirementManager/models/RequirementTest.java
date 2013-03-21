95494746fbde6629a7f98fe8ed4c8afda8d21441
David Mihal <eastsideski@gmail.com>
2013-03-20 22:18:45 -0400

    Improved Requirement test cases

--- RequirementManager/test/edu/wpi/cs/wpisuitetng/modules/RequirementManager/models/RequirementTest.java
+++ RequirementManager/test/edu/wpi/cs/wpisuitetng/modules/RequirementManager/models/RequirementTest.java
@@ -16,16 +16,26 @@ public class RequirementTest {
 
 	@Test
 	  public void jSONConversionTests() {
-	    Requirement object = new Requirement(4, "Test",
-	        "1.1.01", RequirementStatus.NEW , RequirementPriority.MEDIUM, "A test", 1, 10);
+	    Requirement object = new Requirement(4, "Test", "A test");
+	    
+	    assertEquals(object.getStatus(),RequirementStatus.NEW);
+	    assertEquals(object.getPriority(),RequirementPriority.BLANK);
+	    
+	    object.setRelease("1.1.01");
+	    object.setStatus(RequirementStatus.INPROGRESS);
+	    object.setPriority(RequirementPriority.MEDIUM);
+	    object.setEffort(10);
+	    object.setEstimate(1);
+	    
 	    Requirement origObject = object;  // change here
 	    String jsonMessage = object.toJSON();
 	    Requirement newObject = Requirement.fromJson(jsonMessage);  // change here...
 	    assertTrue(newObject instanceof Requirement);
+	    
 	    assertEquals(origObject.getId(), 4);
 	    assertEquals(origObject.getName(), "Test");
 	    assertEquals(origObject.getRelease(), "1.1.01");
-	    assertEquals(origObject.getStatus(), RequirementStatus.NEW);
+	    assertEquals(origObject.getStatus(), RequirementStatus.INPROGRESS);
 	    assertEquals(origObject.getPriority(), RequirementPriority.MEDIUM);
 	    assertEquals(origObject.getDescription(), "A test");
 	    assertEquals(origObject.getEstimate(), 1);
