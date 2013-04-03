79b3a58e8f95b5f34738846e360d362dbb4d500a
David Mihal <eastsideski@gmail.com>
2013-04-02 22:27:05 -0400

    Working Note Displays!

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementManager/view/Requirements/NotePanel.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/RequirementManager/view/Requirements/NotePanel.java
@@ -6,6 +6,7 @@ package edu.wpi.cs.wpisuitetng.modules.RequirementManager.view.Requirements;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -35,32 +36,37 @@ public class NotePanel extends JPanel {
 	 */
 	public NotePanel(Note note)
 	{
-		this.setBorder(BorderFactory.createLineBorder(Color.black));
+		this.setBorder(BorderFactory.createLineBorder(Color.black)); //Set note border
 		
 		JTextArea message = new JTextArea(note.getMessage());
 		message.setEditable(false);
-		message.setBorder(BorderFactory.createLineBorder(Color.black));
-		//JLabel message = new JLabel(note.getMessage());
+		// Give the message a black border with 2px padding inside
+		Border b = BorderFactory.createCompoundBorder(
+				BorderFactory.createLineBorder(Color.black), 
+	            BorderFactory.createEmptyBorder(2, 2, 2, 2));
+		message.setBorder(b);
+
 		String user = note.getUser();
 		
 		Date date = new Date(note.getTimestamp());
 		Format format = new SimpleDateFormat("MMMMM d, yyyy 'at' hh:mm:ss aaa");
 		String noteDate = format.format(date).toString();
 		
-		String info = user + " on " + noteDate;
-		JLabel noteInfo = new JLabel(info);
+		JLabel noteInfo = new JLabel(user + " on " + noteDate);
 		
 		// SpringLayout noteLayout = new SpringLayout();
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints noteConstraints = new GridBagConstraints();
 		
 		noteConstraints.fill = GridBagConstraints.HORIZONTAL;
-		noteConstraints.gridy = 0;
+		noteConstraints.gridy = 0; //Row 0
+		noteConstraints.weightx = 1; //Fill the width
+		noteConstraints.insets = new Insets(2,2,2,2); //2px margin
 		this.add(message, noteConstraints);
 		
 		noteConstraints.anchor = GridBagConstraints.SOUTHEAST;
 		noteConstraints.fill = GridBagConstraints.NONE;
-		noteConstraints.gridy = 1;
+		noteConstraints.gridy = 1; //Row 2
 		this.add(noteInfo, noteConstraints);
 	}
 	
@@ -73,15 +79,23 @@ public class NotePanel extends JPanel {
 		c.gridy = GridBagConstraints.RELATIVE;
 		c.anchor = GridBagConstraints.NORTH;
 		c.fill = GridBagConstraints.HORIZONTAL;
+		c.weightx = 1;//Fill horizontally
+		c.gridy = 0; //Row 0
+		c.insets = new Insets(5,5,5,5);
 		
 		ListIterator<Note> itt = list.getIterator(0);
 		while(itt.hasNext())
 		{
+			//Create a new NotePanel for each Note and add it to the panel
 			panel.add(new NotePanel(itt.next()),c);
+			c.gridy++; //Next Row
 		}
-		c.weightx = 1;
+		
+		//Create a dummy panel to take up space at the bottom
 		c.weighty = 1;
-		panel.add(new JPanel(),c);
+		JPanel dummy = new JPanel();
+		dummy.setBackground(Color.WHITE);
+		panel.add(dummy,c);
 		
 		return panel;
 	}
