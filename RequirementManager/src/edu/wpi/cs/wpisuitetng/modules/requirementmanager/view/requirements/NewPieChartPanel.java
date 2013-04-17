ade7b10cac2ca55beaa31328bedef8e1e853aff6
David <david@david-XPS-L501X.(none)>
2013-04-16 23:47:33 -0400

    Unstable version pushed to include proper build paths for Jcommon and Jfreechart jars

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/view/requirements/NewPieChartPanel.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/view/requirements/NewPieChartPanel.java
@@ -3,37 +3,17 @@
  */
 package edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.requirements;
 
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.KeyEvent;
-import java.beans.PropertyChangeEvent;
 
-
-
-import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.SpringLayout;
 
-import org.jfree.chart.*;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PiePlot;
-import org.jfree.data.general.*;
-
-
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
 import org.jfree.ui.ApplicationFrame;
-
-import java.util.Date;
-
-import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
-import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.RequirementModel;
-import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.characteristics.RequirementPriority;
-import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.characteristics.RequirementStatus;
-import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.characteristics.RequirementType;
-import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.characteristics.TransactionHistory;
-import edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.ViewEventController;
 
 /**
  * @author Westley
