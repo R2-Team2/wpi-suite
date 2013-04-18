6c79880cac21b621b4f519513d7b559a106690a7
wdrussell <wdrussell@wpi.edu>
2013-04-17 23:43:33 -0400

    Pie chart now displays a pie chart of status

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/view/requirements/NewPieChartPanel.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/view/requirements/NewPieChartPanel.java
@@ -6,6 +6,7 @@ package edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.requirements;
 import java.awt.Font;
 import java.awt.event.ItemEvent;
 import java.awt.event.KeyEvent;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -15,9 +16,15 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.plot.PiePlot3D;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
 import org.jfree.ui.ApplicationFrame;
+import org.jfree.util.Rotation;
+
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.RequirementModel;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.characteristics.RequirementStatus;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.ViewEventController;
 
 /**
@@ -36,27 +43,52 @@ public class NewPieChartPanel extends RequirementPanel{
 	}
 	
 	private static PieDataset setData(){
+		int numStatusNew = 0;
+		int numStatusDeleted = 0;
+		int numStatusInprogress = 0;
+		int numStatusComplete = 0;
+		int numStatusOpen = 0;
 		DefaultPieDataset dataSet = new DefaultPieDataset();
-		dataSet.setValue("One", 1);
-		dataSet.setValue("Two", 2);
-		dataSet.setValue("Three", 3);
+		List<Requirement> requirements = RequirementModel.getInstance().getRequirements();
+		for(int i = 0; i < requirements.size(); i++){
+			if(requirements.get(i).getStatus() == RequirementStatus.NEW){
+				numStatusNew += 1;
+			}
+			else if(requirements.get(i).getStatus() == RequirementStatus.DELETED){
+				numStatusDeleted += 1;
+			}
+			else if(requirements.get(i).getStatus() == RequirementStatus.INPROGRESS){
+				numStatusInprogress += 1;
+			}
+			else if(requirements.get(i).getStatus() == RequirementStatus.COMPLETE){
+				numStatusComplete += 1;
+			}
+			else{
+				numStatusOpen += 1;
+			}
+		}
+		dataSet.setValue("New", numStatusNew);
+		dataSet.setValue("Deleted", numStatusDeleted);
+		dataSet.setValue("In Progress", numStatusInprogress);
+		dataSet.setValue("Complete", numStatusComplete);
+		dataSet.setValue("Open", numStatusOpen);
 		return dataSet;
 	}
 	
 private static JFreeChart createChart(PieDataset dataset) {
         
-        JFreeChart chart = ChartFactory.createPieChart(
-            "Pie Chart Demo 1",  // chart title
+        JFreeChart chart = ChartFactory.createPieChart3D(
+            "Pie Chart Status",  // chart title
             dataset,             // data
             true,               // include legend
             true,
             false
         );
 
-        PiePlot plot = (PiePlot) chart.getPlot();
+        PiePlot3D plot = (PiePlot3D) chart.getPlot();
         plot.setLabelFont(new Font("SansSerif", Font.PLAIN, 12));
         plot.setNoDataMessage("No data available");
-        plot.setCircular(false);
+        plot.setCircular(true);
         plot.setLabelGap(0.02);
         return chart;
         
