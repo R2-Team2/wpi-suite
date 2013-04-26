c86bde62a38ca191c415658448c73ae8787bf09a
David Mihal <eastsideski@gmail.com>
2013-04-25 22:42:07 -0400

    Added scroll bar to note field, fixing #50

--- RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/view/requirements/tabs/RequirementNotePanel.java
+++ RequirementManager/src/edu/wpi/cs/wpisuitetng/modules/requirementmanager/view/requirements/tabs/RequirementNotePanel.java
@@ -10,6 +10,7 @@
 package edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.requirements.tabs;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -56,7 +57,7 @@ public class RequirementNotePanel extends JPanel implements RequirementPanelList
 		viewMode = vm;
 		notesAdded = 0;
 		
-		this.buildNoteMessage();
+		Component noteField = this.buildNoteField();
 		noteScroll = new JScrollPane();
 
 		// Buttons to be added to the bottom of the NotePanel
@@ -90,7 +91,7 @@ public class RequirementNotePanel extends JPanel implements RequirementPanelList
 
 		c.gridy = 1; // Row 1
 		c.weighty = 0; // Fill 0% of vertical space
-		this.add(noteMessage, c);
+		this.add(noteField, c);
 
 		bc.anchor = GridBagConstraints.WEST; // Anchor buttons to west of bottom
 												// panel
@@ -173,7 +174,7 @@ public class RequirementNotePanel extends JPanel implements RequirementPanelList
 	 * Method buildNoteMessage.
 	 * @return JTextArea
 	 */
-	private JTextArea buildNoteMessage(){		
+	private Component buildNoteField(){		
 		noteMessage.addKeyListener(new KeyAdapter()
 		{
 			@Override
@@ -196,7 +197,11 @@ public class RequirementNotePanel extends JPanel implements RequirementPanelList
 	            BorderFactory.createEmptyBorder(4, 4, 4, 4));
 		noteMessage.setBorder(b);
 		
-		return noteMessage;
+		JScrollPane scroller = new JScrollPane(noteMessage);
+		scroller.setMinimumSize(new Dimension(50,50));
+		scroller.setPreferredSize(new Dimension(60,60));
+		
+		return scroller;
 	}
 
 	/**
