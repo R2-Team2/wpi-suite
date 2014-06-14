3af3c979b846d15c3c0f6ef061e07e71ef9b3761
Robert Smieja <robertsmieja@gmail.com>
2014-06-13 20:56:29 -0400

    Fix errors from moving images to resources
    -Some formatting changes and import organization

--- RequirementManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/requirementmanager/view/buttons/ChartButtonsPanel.java
+++ RequirementManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/requirementmanager/view/buttons/ChartButtonsPanel.java
@@ -1,11 +1,9 @@
 /**
- *  * Copyright (c) 2013 -- WPI Suite
- *
+ * * Copyright (c) 2013 -- WPI Suite
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
  * Contributors:
  */
 package edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.buttons;
@@ -20,14 +18,10 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
-import javax.swing.AbstractButton;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.SpringLayout;
-import javax.swing.border.EtchedBorder;
 
 import edu.wpi.cs.wpisuitetng.janeway.gui.container.toolbar.ToolbarGroupView;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.ViewEventController;
@@ -36,48 +30,47 @@ import edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.ViewEventControlle
  * @author justinhess
  * @version $Revision: 1.0 $
  */
-public class ChartButtonsPanel extends ToolbarGroupView{
+public class ChartButtonsPanel extends ToolbarGroupView {
 	
 	private final JPanel contentPanel = new JPanel();
 	
 	JButton pieChart;
 	JButton barChart;
 	
-	public ChartButtonsPanel(){
+	public ChartButtonsPanel() {
 		super("");
 		
 		this.contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.X_AXIS));
 		
 		JButton pieChart = new JButton("<html>View Pie<br />Chart</html>");
 		
-		pieChart.addActionListener(new ActionListener(){
+		pieChart.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e){
+			public void actionPerformed(ActionEvent e) {
 				ViewEventController.getInstance().createPieChart("Status");
 			}
 		});
 		
 		JButton barChart = new JButton("<html>View Bar<br />Chart</html>");
 		
-		barChart.addActionListener(new ActionListener(){
+		barChart.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e){
+			public void actionPerformed(ActionEvent e) {
 				ViewEventController.getInstance().createBarChart("Status");
-				
-				
-				
 				
 			}
 		});
 		
 		try {
-		    Image img = ImageIO.read(getClass().getResource("pie_chart.png"));
-		    pieChart.setIcon(new ImageIcon(img));
-		    
-		    img = ImageIO.read(getClass().getResource("bar_chart.png"));
-		    barChart.setIcon(new ImageIcon(img));
-		    
-		} catch (IOException ex) {}
+			Image img = ImageIO.read(this.getClass().getResourceAsStream("pie_chart.png"));
+			pieChart.setIcon(new ImageIcon(img));
+			
+			img = ImageIO.read(this.getClass().getResourceAsStream("bar_chart.png"));
+			barChart.setIcon(new ImageIcon(img));
+			
+		} catch (IOException ex) {
+			System.out.println(ex.getStackTrace());
+		}
 		
 		contentPanel.add(pieChart);
 		contentPanel.add(barChart);
@@ -85,17 +78,19 @@ public class ChartButtonsPanel extends ToolbarGroupView{
 		
 		this.add(contentPanel);
 	}
-
+	
 	/**
 	 * Method getBarChartButton.
+	 * 
 	 * @return JButton
 	 */
 	public JButton getBarChartButton() {
 		return barChart;
 	}
-
+	
 	/**
 	 * Method getPieChartButton.
+	 * 
 	 * @return JButton
 	 */
 	public JButton getPieChartButton() {
--- RequirementManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/requirementmanager/view/buttons/RequirementButtonsPanel.java
+++ RequirementManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/requirementmanager/view/buttons/RequirementButtonsPanel.java
@@ -4,27 +4,21 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
  * Contributors: Team Rolling Thunder
  ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.buttons;
 
-import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
-import javax.swing.AbstractButton;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.SpringLayout;
 import javax.swing.SwingConstants;
-import javax.swing.border.EtchedBorder;
 
 import edu.wpi.cs.wpisuitetng.janeway.gui.container.toolbar.ToolbarGroupView;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.ViewEventController;
@@ -33,16 +27,17 @@ import edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.ViewEventControlle
  * @author justinhess
  * @version $Revision: 1.0 $
  */
-public class RequirementButtonsPanel extends ToolbarGroupView{
+@SuppressWarnings("serial")
+public class RequirementButtonsPanel extends ToolbarGroupView {
 	
 	// initialize the main view toolbar buttons
-		private JButton createButton = new JButton("<html>Create<br />Requirement</html>");
-		
-		private final JButton createIterationButton = new JButton("<html>Create<br />Iteration</html>");
-		private final JPanel contentPanel = new JPanel();
+	private JButton createButton = new JButton("<html>Create<br />Requirement</html>");
 	
-	public RequirementButtonsPanel(){
-		super("");
+	private final JButton createIterationButton = new JButton("<html>Create<br />Iteration</html>");
+	private final JPanel contentPanel = new JPanel();
+	
+	public RequirementButtonsPanel() {
+		super(""); //TODO What is this??? I'm so confused. Is this dead code?
 		
 		this.contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.X_AXIS));
 		this.setPreferredWidth(350);
@@ -51,13 +46,15 @@ public class RequirementButtonsPanel extends ToolbarGroupView{
 		//this.createButton.setPreferredSize(new Dimension(200, 200));
 		this.createButton.setHorizontalAlignment(SwingConstants.CENTER);
 		try {
-		    Image img = ImageIO.read(getClass().getResource("new_req.png"));
-		    this.createButton.setIcon(new ImageIcon(img));
-		    
-		    img = ImageIO.read(getClass().getResource("new_itt.png"));
-		    this.createIterationButton.setIcon(new ImageIcon(img));
-		    
-		} catch (IOException ex) {}
+			Image img = ImageIO.read(this.getClass().getResourceAsStream("new_req.png"));
+			this.createButton.setIcon(new ImageIcon(img));
+			
+			img = ImageIO.read(this.getClass().getResourceAsStream("new_req.png"));
+			this.createIterationButton.setIcon(new ImageIcon(img));
+			
+		} catch (IOException ex) {
+			//TODO why are we swallowing exceptions???
+		}
 		
 		// the action listener for the Create Requirement Button
 		createButton.addActionListener(new ActionListener() {
@@ -65,43 +62,44 @@ public class RequirementButtonsPanel extends ToolbarGroupView{
 			public void actionPerformed(ActionEvent e) {
 				// bring up a create requirement pane if not in Multiple Requirement Editing Mode
 				//if (!ViewEventController.getInstance().getOverviewTable().getEditFlag()) {
-					ViewEventController.getInstance().createRequirement();
-			//	}
+				ViewEventController.getInstance().createRequirement();
+				//	}
 			}
-		});		
+		});
 		
 		//action listener for the Create Iteration Button
 		createIterationButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				//if (!ViewEventController.getInstance().getOverviewTable().getEditFlag()) {
-					ViewEventController.getInstance().createIteration();
-				}
-		//	}
+				ViewEventController.getInstance().createIteration();
+			}
+			//	}
 		});
-			
+		
 		contentPanel.add(createButton);
 		contentPanel.add(createIterationButton);
 		contentPanel.setOpaque(false);
 		
-
 		this.add(contentPanel);
 	}
+	
 	/**
 	 * Method getCreateButton.
-	
-	 * @return JButton */
+	 * 
+	 * @return JButton
+	 */
 	public JButton getCreateButton() {
 		return createButton;
 	}
-
+	
 	/**
 	 * Method getCreateIterationButton.
-	
-	 * @return JButton */
+	 * 
+	 * @return JButton
+	 */
 	public JButton getCreateIterationButton() {
 		return createIterationButton;
 	}
-
 	
 }
