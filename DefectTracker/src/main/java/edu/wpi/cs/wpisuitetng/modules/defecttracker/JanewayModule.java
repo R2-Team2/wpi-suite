dbaabb77027c973bf64e8a5693bf674510ef9601
Robert Smieja <robertsmieja@gmail.com>
2014-08-27 18:57:05 -0400

    Finish TestJanewayModule for DefectTracker
    100% code coverage

--- DefectTracker/src/main/java/edu/wpi/cs/wpisuitetng/modules/defecttracker/JanewayModule.java
+++ DefectTracker/src/main/java/edu/wpi/cs/wpisuitetng/modules/defecttracker/JanewayModule.java
@@ -51,7 +51,7 @@ public class JanewayModule implements IJanewayModule {
 
         // add keyboard shortcuts to defects tab
         String osName = System.getProperty("os.name").toLowerCase();
-        registerKeyboardShortcuts(tab, osName);
+        registerKeyboardShortcuts(mainTabController, tab, osName);
     }
 
     /**
@@ -71,7 +71,7 @@ public class JanewayModule implements IJanewayModule {
     }
 
     @SuppressWarnings("serial")
-    protected void registerKeyboardShortcuts(JanewayTabModel tab, String osName) {
+    protected void registerKeyboardShortcuts(MainTabController mainTabController, JanewayTabModel tab, String osName) {
 
         // control + tab: switch to right tab
         tab.addKeyboardShortcut(new KeyboardShortcut(KeyStroke.getKeyStroke("control TAB"), new AbstractAction() {
--- DefectTracker/src/test/java/edu/wpi/cs/wpisuitetng/modules/defecttracker/TestJanewayModule.java
+++ DefectTracker/src/test/java/edu/wpi/cs/wpisuitetng/modules/defecttracker/TestJanewayModule.java
@@ -15,6 +15,7 @@ import org.mockito.ArgumentCaptor;
 
 import edu.wpi.cs.wpisuitetng.janeway.gui.widgets.KeyboardShortcut;
 import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
+import edu.wpi.cs.wpisuitetng.modules.defecttracker.tabs.MainTabController;
 import edu.wpi.cs.wpisuitetng.modules.defecttracker.tabs.MainTabView;
 import edu.wpi.cs.wpisuitetng.modules.defecttracker.toolbar.ToolbarView;
 
@@ -47,8 +48,6 @@ public class TestJanewayModule {
         assertNotNull(tab.getIcon());
         assertEquals(toolbarView, tab.getToolbar());
         assertEquals(mainTabView, tab.getMainComponent());
-
-        //TODO Register KeyboardShortcut
     }
 
     @Test
@@ -59,38 +58,53 @@ public class TestJanewayModule {
     @Test
     public void testRegisterKeyboardShortcut_Mac() {
         JanewayTabModel mockTab = mock(JanewayTabModel.class);
+        MainTabController mockMainTabController = mock(MainTabController.class);
         String osName = "some kind of mac";
         ArgumentCaptor<KeyboardShortcut> keyboardShortcutCaptor = ArgumentCaptor.forClass(KeyboardShortcut.class);
 
-        janewayModule.registerKeyboardShortcuts(mockTab, osName);
+        janewayModule.registerKeyboardShortcuts(mockMainTabController, mockTab, osName);
 
         verify(mockTab, times(3)).addKeyboardShortcut(keyboardShortcutCaptor.capture());
 
         List<KeyboardShortcut> keyboardShortcuts = keyboardShortcutCaptor.getAllValues();
 
         assertEquals(KeyStroke.getKeyStroke("control TAB"), keyboardShortcuts.get(0).getKeyStroke());
+        keyboardShortcuts.get(0).getActions().get(0).actionPerformed(null);
+        verify(mockMainTabController, times(1)).switchToRightTab();
 
         assertEquals(KeyStroke.getKeyStroke("control shift TAB"), keyboardShortcuts.get(1).getKeyStroke());
+        keyboardShortcuts.get(1).getActions().get(0).actionPerformed(null);
+        verify(mockMainTabController, times(1)).switchToLeftTab();
 
         assertEquals(KeyStroke.getKeyStroke("meta W"), keyboardShortcuts.get(2).getKeyStroke());
+        keyboardShortcuts.get(2).getActions().get(0).actionPerformed(null);
+        verify(mockMainTabController, times(1)).closeCurrentTab();
     }
 
     @Test
     public void testRegisterKeyboardShortcut_NotMac() {
         JanewayTabModel mockTab = mock(JanewayTabModel.class);
+        MainTabController mockMainTabController = mock(MainTabController.class);
         String osName = "archlinux because I can";
         ArgumentCaptor<KeyboardShortcut> keyboardShortcutCaptor = ArgumentCaptor.forClass(KeyboardShortcut.class);
 
-        janewayModule.registerKeyboardShortcuts(mockTab, osName);
+        janewayModule.registerKeyboardShortcuts(mockMainTabController, mockTab, osName);
 
         verify(mockTab, times(3)).addKeyboardShortcut(keyboardShortcutCaptor.capture());
 
         List<KeyboardShortcut> keyboardShortcuts = keyboardShortcutCaptor.getAllValues();
 
+        //First action
         assertEquals(KeyStroke.getKeyStroke("control TAB"), keyboardShortcuts.get(0).getKeyStroke());
+        keyboardShortcuts.get(0).getActions().get(0).actionPerformed(null);
+        verify(mockMainTabController, times(1)).switchToRightTab();
 
         assertEquals(KeyStroke.getKeyStroke("control shift TAB"), keyboardShortcuts.get(1).getKeyStroke());
+        keyboardShortcuts.get(1).getActions().get(0).actionPerformed(null);
+        verify(mockMainTabController, times(1)).switchToLeftTab();
 
         assertEquals(KeyStroke.getKeyStroke("ctrl W"), keyboardShortcuts.get(2).getKeyStroke());
+        keyboardShortcuts.get(2).getActions().get(0).actionPerformed(null);
+        verify(mockMainTabController, times(1)).closeCurrentTab();
     }
 }
