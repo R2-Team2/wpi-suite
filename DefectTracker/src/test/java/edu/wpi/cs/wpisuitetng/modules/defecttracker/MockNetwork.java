6752c9936de636a89e719d09921f34a3c576f80f
Robert Smieja <robertsmieja@gmail.com>
2014-08-27 19:16:21 -0400

    Mark old mocks as deprecated

--- DefectTracker/src/test/java/edu/wpi/cs/wpisuitetng/modules/defecttracker/MockNetwork.java
+++ DefectTracker/src/test/java/edu/wpi/cs/wpisuitetng/modules/defecttracker/MockNetwork.java
@@ -17,22 +17,23 @@ import edu.wpi.cs.wpisuitetng.network.Network;
 import edu.wpi.cs.wpisuitetng.network.Request;
 import edu.wpi.cs.wpisuitetng.network.models.HttpMethod;
 
+@Deprecated
 public class MockNetwork extends Network {
-	
-	protected MockRequest lastRequestMade = null;
-	
-	@Override
-	public Request makeRequest(String path, HttpMethod requestMethod) {
-		if (requestMethod == null) {
-			throw new NullPointerException("requestMethod may not be null");
-		}
-		
-		lastRequestMade = new MockRequest(defaultNetworkConfiguration, path, requestMethod); 
-		
-		return lastRequestMade;
-	}
-	
-	public MockRequest getLastRequestMade() {
-		return lastRequestMade;
-	}
+
+    protected MockRequest lastRequestMade = null;
+
+    @Override
+    public Request makeRequest(String path, HttpMethod requestMethod) {
+        if (requestMethod == null) {
+            throw new NullPointerException("requestMethod may not be null");
+        }
+
+        lastRequestMade = new MockRequest(defaultNetworkConfiguration, path, requestMethod);
+
+        return lastRequestMade;
+    }
+
+    public MockRequest getLastRequestMade() {
+        return lastRequestMade;
+    }
 }
