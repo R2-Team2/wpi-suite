3edd3ecdfc159504bea97627a595e32bc2d04fa9
Robert Smieja <robertsmieja@gmail.com>
2014-08-27 18:51:43 -0400

    Add start of registerKeyboardShortcuts tests

--- DefectTracker/src/test/java/edu/wpi/cs/wpisuitetng/modules/defecttracker/TestJanewayModule.java
+++ DefectTracker/src/test/java/edu/wpi/cs/wpisuitetng/modules/defecttracker/TestJanewayModule.java
@@ -2,9 +2,18 @@ package edu.wpi.cs.wpisuitetng.modules.defecttracker;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.List;
+
+import javax.swing.KeyStroke;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
+import edu.wpi.cs.wpisuitetng.janeway.gui.widgets.KeyboardShortcut;
 import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
 import edu.wpi.cs.wpisuitetng.modules.defecttracker.tabs.MainTabView;
 import edu.wpi.cs.wpisuitetng.modules.defecttracker.toolbar.ToolbarView;
@@ -45,5 +54,43 @@ public class TestJanewayModule {
     @Test
     public void testGetName() {
         assertEquals("Defect Tracker", janewayModule.getName());
+    }
+
+    @Test
+    public void testRegisterKeyboardShortcut_Mac() {
+        JanewayTabModel mockTab = mock(JanewayTabModel.class);
+        String osName = "some kind of mac";
+        ArgumentCaptor<KeyboardShortcut> keyboardShortcutCaptor = ArgumentCaptor.forClass(KeyboardShortcut.class);
+
+        janewayModule.registerKeyboardShortcuts(mockTab, osName);
+
+        verify(mockTab, times(3)).addKeyboardShortcut(keyboardShortcutCaptor.capture());
+
+        List<KeyboardShortcut> keyboardShortcuts = keyboardShortcutCaptor.getAllValues();
+
+        assertEquals(KeyStroke.getKeyStroke("control TAB"), keyboardShortcuts.get(0).getKeyStroke());
+
+        assertEquals(KeyStroke.getKeyStroke("control shift TAB"), keyboardShortcuts.get(1).getKeyStroke());
+
+        assertEquals(KeyStroke.getKeyStroke("meta W"), keyboardShortcuts.get(2).getKeyStroke());
+    }
+
+    @Test
+    public void testRegisterKeyboardShortcut_NotMac() {
+        JanewayTabModel mockTab = mock(JanewayTabModel.class);
+        String osName = "archlinux because I can";
+        ArgumentCaptor<KeyboardShortcut> keyboardShortcutCaptor = ArgumentCaptor.forClass(KeyboardShortcut.class);
+
+        janewayModule.registerKeyboardShortcuts(mockTab, osName);
+
+        verify(mockTab, times(3)).addKeyboardShortcut(keyboardShortcutCaptor.capture());
+
+        List<KeyboardShortcut> keyboardShortcuts = keyboardShortcutCaptor.getAllValues();
+
+        assertEquals(KeyStroke.getKeyStroke("control TAB"), keyboardShortcuts.get(0).getKeyStroke());
+
+        assertEquals(KeyStroke.getKeyStroke("control shift TAB"), keyboardShortcuts.get(1).getKeyStroke());
+
+        assertEquals(KeyStroke.getKeyStroke("ctrl W"), keyboardShortcuts.get(2).getKeyStroke());
     }
 }
