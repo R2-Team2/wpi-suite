6d58990b94552e206f6ff6ed15c860de3f1b56a3
Robert Smieja <robertsmieja@gmail.com>
2014-09-03 17:48:16 -0400

    Improve BasicAuth code coverage to 100%

--- Core/WPISuite/src/test/java/edu/wpi/cs/wpisuitetng/authentication/TestBasicAuth.java
+++ Core/WPISuite/src/test/java/edu/wpi/cs/wpisuitetng/authentication/TestBasicAuth.java
@@ -12,70 +12,95 @@
 
 package edu.wpi.cs.wpisuitetng.authentication;
 
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-import edu.wpi.cs.wpisuitetng.authentication.BasicAuth;
-import edu.wpi.cs.wpisuitetng.exceptions.AuthenticationException;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.apache.commons.codec.binary.Base64;
+import org.junit.Test;
+
+import edu.wpi.cs.wpisuitetng.exceptions.AuthenticationException;
 
 public class TestBasicAuth {
-	String header = "Authorization: Basic "; // the static BasicAuth header
-	BasicAuth basic;
-	
-	@Before
-	public void setUp()
-	{
-		this.basic = new BasicAuth();
-	}
-	
-	
-	@Test(expected=AuthenticationException.class)
-	/**
-	 * When given an improperly formatted POST-string (not in 3 parts)
-	 * 	then expect an exception
-	 * @throws AuthenticationException
-	 */
-	public void testInvalidTokenFormat() throws AuthenticationException
-	{
-		this.basic.parsePost("bahston");
-	}
-	
-	@Test(expected=AuthenticationException.class)
-	/**
-	 * Generates a valid BasicAuth token with invalidly formatted
-	 * 	credentials. Decoding should work properly. An exception should
-	 * 	raise because the credential string is not in the  "String:String" format.
-	 */
-	public void testInvalidCredentialFormat() throws AuthenticationException
-	{
-		String authToken = "Basic ";
-		String credentials = "abcdefg123456";
-		authToken += Base64.encodeBase64String(credentials.getBytes());
-		
-		this.basic.parsePost(authToken);
-	}
-	
-	@Test
-	/**
-	 * Tests a situation when a valid BasicAuth token is given
-	 * 	to the POST string parser.
-	 * @throws AuthenticationException
-	 */
-	public void testValidFormat() throws AuthenticationException
-	{
-		String username = "twack";
-		String pass = "12345";
-		
-		String basicAuthToken = BasicAuth.generateBasicAuth(username, pass);
-		String[] credentials = this.basic.parsePost(basicAuthToken);
-		
-		assertEquals(credentials.length, 2);
-		assertTrue(credentials[0].equals(username));
-		assertTrue(credentials[1].equals(pass));
-	}
-	
-	
+    String header = "Authorization: Basic "; // the static BasicAuth header
+    BasicAuth basic = new BasicAuth();
+
+    /**
+     * When given an improperly formatted POST-string (not in 3 parts)
+     * then expect an exception
+     * 
+     * @throws AuthenticationException
+     */
+    @Test(expected = AuthenticationException.class)
+    public void testInvalidTokenFormat() throws AuthenticationException
+    {
+        this.basic.parsePost("bahston");
+    }
+
+    /**
+     * Generates a valid BasicAuth token with invalidly formatted
+     * credentials. Decoding should work properly. An exception should
+     * raise because the credential string is not in the "String:String" format.
+     */
+    @Test(expected = AuthenticationException.class)
+    public void testInvalidCredentialFormat() throws AuthenticationException
+    {
+        String authToken = "Basic ";
+        String credentials = "abcdefg123456";
+        authToken += Base64.encodeBase64String(credentials.getBytes());
+
+        this.basic.parsePost(authToken);
+    }
+
+    /**
+     * Tests a situation when a valid BasicAuth token is given
+     * to the POST string parser.
+     * 
+     * @throws AuthenticationException
+     */
+    @Test
+    public void testValidFormat() throws AuthenticationException
+    {
+        String username = "twack";
+        String pass = "12345";
+
+        String basicAuthToken = BasicAuth.generateBasicAuth(username, pass);
+        String[] credentials = this.basic.parsePost(basicAuthToken);
+
+        assertEquals(credentials.length, 2);
+        assertTrue(credentials[0].equals(username));
+        assertTrue(credentials[1].equals(pass));
+    }
+
+    @Test
+    public void TestIsValidBasicAuth()
+    {
+        String[] basicAuth = { "Basic", "QmFzZTY0" };
+
+        assertTrue(basic.isValidBasicAuth(basicAuth));
+    }
+
+    @Test
+    public void TestIsValidBasicAuth_IncorrectFormat_Length()
+    {
+        String[] basicAuth = { "NotBasic" };
+
+        assertFalse(basic.isValidBasicAuth(basicAuth));
+    }
+
+    @Test
+    public void TestIsValidBasicAuth_IncorrectFormat_NotBasic()
+    {
+        String[] basicAuth = { "NotBasic", "##NotBase64##" };
+
+        assertFalse(basic.isValidBasicAuth(basicAuth));
+    }
+
+    @Test
+    public void TestIsValidBasicAuth_NotBase64()
+    {
+        String[] basicAuth = { "Basic", "##NotBase64##" };
+
+        assertFalse(basic.isValidBasicAuth(basicAuth));
+    }
 }
