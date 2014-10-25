ce8b479c8e10788f369d264e88a7c1d8f9da2a2d
Robert Smieja <robertsmieja@gmail.com>
2014-10-25 17:30:21 -0400

    Fix IllegalArgumentException in Unit Test
    Reported by Gradle but not by Eclipse

--- RequirementManager/src/test/java/edu/wpi/cs/wpisuitetng/modules/requirementmanager/view/requirements/TestRequirementInformationPanel.java
+++ RequirementManager/src/test/java/edu/wpi/cs/wpisuitetng/modules/requirementmanager/view/requirements/TestRequirementInformationPanel.java
@@ -2,16 +2,20 @@ package edu.wpi.cs.wpisuitetng.modules.requirementmanager.view.requirements;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.awt.Dimension;
+
+import javax.swing.DefaultListModel;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
+import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.RequirementModel;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.characteristics.RequirementStatus;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.iterations.Iteration;
 import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.iterations.IterationModel;
@@ -24,6 +28,7 @@ public class TestRequirementInformationPanel {
     private IterationModel mockIterationModel = mock(IterationModel.class);
     private ViewEventController mockViewEventController = mock(ViewEventController.class);
     private Requirement mockRequirement = mock(Requirement.class);
+    private RequirementModel mockRequirementModel = mock(RequirementModel.class);
     private RequirementPanel mockRequirementPanel = mock(RequirementPanel.class);
     private ViewMode mode = ViewMode.EDITING;
 
@@ -31,15 +36,17 @@ public class TestRequirementInformationPanel {
     public void setup() {
         when(mockRequirement.getParentID()).thenReturn(-1);
         when(mockRequirement.getStatus()).thenReturn(RequirementStatus.NEW);
+        when(mockRequirementModel.getPossibleParents(any(Requirement.class))).thenReturn(new DefaultListModel<Requirement>());
         when(mockIteration.toString()).thenReturn("");
         when(mockIterationModel.getBacklog()).thenReturn(mockIteration);
 
+        RequirementModel.setInstance(mockRequirementModel);
         IterationModel.setInstance(mockIterationModel);
         ViewEventController.setInstance(mockViewEventController);
     }
 
     @Test
-    public void testRequirementInformationPanel() {
+    public void testConstructor() {
 
         RequirementInformationPanel newPanel = new RequirementInformationPanel(mockRequirementPanel, mode, mockRequirement);
 
