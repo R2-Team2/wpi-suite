1f6a09ff005cc2d536ae1043318232d31d5460c8
Robert Smieja <robertsmieja@gmail.com>
2014-10-25 23:17:15 -0400

    Fix issue when editing a Requirement
    -The buttons didn't appear until an error occurred.
    -I hate people who swallow exceptions in empty catch blocks

--- RequirementManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/requirementmanager/view/requirements/RequirementButtonPanel.java
+++ RequirementManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/requirementmanager/view/requirements/RequirementButtonPanel.java
@@ -27,7 +27,7 @@ import edu.wpi.cs.wpisuitetng.modules.requirementmanager.models.Requirement;
  * @version $Revision: 1.0 $
  */
 public class RequirementButtonPanel extends JPanel implements RequirementPanelListener {
-    private static final long serialVersionUID = -8848649876156034937L;
+    private static final long serialVersionUID = -3384610665685796068L;
 
     private final RequirementPanel parentPanel;
     private final ViewMode viewMode;
@@ -42,11 +42,6 @@ public class RequirementButtonPanel extends JPanel implements RequirementPanelLi
     private boolean changes;
     private boolean valid;
 
-    private final String deleteString = "Delete";
-    private final String cancelString = "Cancel";
-    private String okString;
-    private String clearString;
-
     /**
      * Constructor for the requirement button panel
      * 
@@ -55,51 +50,54 @@ public class RequirementButtonPanel extends JPanel implements RequirementPanelLi
      * @param curr current requirement
      */
     public RequirementButtonPanel(RequirementPanel parentPanel, ViewMode mode, Requirement curr) {
-        //Curr is unused?
-
         this.setLayout(new FlowLayout(FlowLayout.LEFT));
-        this.changes = false;
-        this.valid = false;
-        this.errorDisplay = new ErrorPanel();
+        changes = false;
+        valid = false;
+        errorDisplay = new ErrorPanel();
         this.parentPanel = parentPanel;
-        this.viewMode = mode;
+        viewMode = mode;
 
+        String okString;
+        final String cancelString = "Cancel";
+        String clearString;
+        final String deleteString = "Delete";
+
+        buttonOK = new JButton();
+        buttonCancel = new JButton(cancelString);
+        buttonClear = new JButton();
+        buttonDelete = new JButton(deleteString);
+
+        this.add(buttonOK);
+        this.add(buttonClear);
         if (viewMode == ViewMode.CREATING) {
             okString = "Create";
             clearString = "Clear";
         } else {
             okString = "Update";
             clearString = "Undo Changes";
+            this.add(buttonDelete);
         }
 
-        buttonOK = new JButton(okString);
-        buttonCancel = new JButton(cancelString);
-        buttonClear = new JButton(clearString);
-        buttonDelete = new JButton(deleteString);
-
-        buttonOK.setEnabled(false);
-        buttonClear.setEnabled(false);
-
         try {
-            Image img = ImageIO.read(getClass().getResource("save-icon.png"));
+            Image img = ImageIO.read(this.getClass().getResourceAsStream("save-icon.png"));
             buttonOK.setIcon(new ImageIcon(img));
 
-            img = ImageIO.read(getClass().getResource("undo-icon.png"));
+            img = ImageIO.read(this.getClass().getResourceAsStream("undo-icon.png"));
             buttonClear.setIcon(new ImageIcon(img));
 
-            img = ImageIO.read(getClass().getResource("delete-icon.png"));
+            img = ImageIO.read(this.getClass().getResourceAsStream("delete-icon.png"));
             buttonDelete.setIcon(new ImageIcon(img));
         } catch (IOException ex) {
-            //TODO: Stop swallowing exceptions
+            //TODO Why are we swallowing exceptions?
             ex.printStackTrace();
         }
 
-        this.add(buttonOK);
-        this.add(buttonClear);
-        this.add(buttonDelete);
+        buttonOK.setText(okString);
+        buttonClear.setText(clearString);
         this.add(buttonCancel);
+        buttonOK.setEnabled(false);
+        buttonClear.setEnabled(false);
         this.add(errorDisplay);
-
         setupListeners();
     }
 
@@ -217,54 +215,5 @@ public class RequirementButtonPanel extends JPanel implements RequirementPanelLi
      */
     public JButton getButtonCancel() {
         return buttonCancel;
-    }
-
-    /**
-     * @return the changes
-     */
-    protected boolean isChanges() {
-        return changes;
-    }
-
-    /**
-     * @param changes the changes to set
-     */
-    protected void setChanges(boolean changes) {
-        this.changes = changes;
-    }
-
-    /**
-     * @return the valid
-     */
-    public boolean isValid() {
-        return valid;
-    }
-
-    /**
-     * @param valid the valid to set
-     */
-    protected void setValid(boolean valid) {
-        this.valid = valid;
-    }
-
-    /**
-     * @return the parentPanel
-     */
-    protected RequirementPanel getParentPanel() {
-        return parentPanel;
-    }
-
-    /**
-     * @return the viewMode
-     */
-    protected ViewMode getViewMode() {
-        return viewMode;
-    }
-
-    /**
-     * @return the errorDisplay
-     */
-    protected ErrorPanel getErrorDisplay() {
-        return errorDisplay;
     }
 }
