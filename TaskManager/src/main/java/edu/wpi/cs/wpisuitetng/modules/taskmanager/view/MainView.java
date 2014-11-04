2a5560a843e71f05f861ea92ec103a9ea6828e84
Gareth Solbeck <gsolbeck@gmail.com>
2014-11-03 21:07:01 -0500

    Add blank Task Manager module with comments
    Note: this commit also includes the controller and model packages from
    PostBoard. They are not currently utilized.

---
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/MainView.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * Chris Casola
+ ******************************************************************************/
+
+package edu.wpi.cs.wpisuitetng.modules.taskmanager.view;
+
+import javax.swing.JPanel;
+
+
+/**
+ * This panel fills the main content area of the tab for this module.
+ * It is currently blank.
+ */
+@SuppressWarnings("serial")
+public class MainView extends JPanel {
+    
+    
+    /**
+     * Construct the panel.
+     */
+    public MainView() {
+    	
+    }
+    
+}
