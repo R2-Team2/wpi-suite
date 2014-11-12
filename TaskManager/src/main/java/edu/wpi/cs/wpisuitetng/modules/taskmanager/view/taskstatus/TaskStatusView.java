7ea1d1d4be63734dc07621290b7421a469aa7d68
Selim Tanriverdi <setanriverdi@wpi.edu>
2014-11-11 22:59:26 -0500

    Add scaling to Work Flow View and Task Status Panes

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/taskstatus/TaskStatusView.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/taskstatus/TaskStatusView.java
@@ -23,6 +23,10 @@ import javax.swing.ScrollPaneConstants;
 
 import java.awt.Font;
 import java.util.ArrayList;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.RowSpec;
 
 public class TaskStatusView extends JPanel {
 
@@ -32,10 +36,10 @@ public class TaskStatusView extends JPanel {
 	 * Create the panel.
 	 */
 	public TaskStatusView(String title) {
-		setLayout(new MigLayout("", "[200px,grow]", "[40px][200px]"));
 		this.taskStatusObj = new TaskStatus(title);
 		
 		String[] ar = {"one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen","twenty"};
+		setLayout(new MigLayout("", "[236px]", "[26px][200px,grow 500]"));
 		
 //		Taskstatus newTaskStatus = new Taskstatus("In Development");
 //		newTaskStatus.setTaskList(ar);
@@ -44,12 +48,12 @@ public class TaskStatusView extends JPanel {
 		txtpnTitle.setEditable(false);
 		txtpnTitle.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		txtpnTitle.setText(this.taskStatusObj.getName());
-		add(txtpnTitle, "cell 0 0,width 150,height 40");
+		add(txtpnTitle, "cell 0 0,alignx left,aligny top");
 		JList scrollableList = new JList(ar);
 		JScrollPane scrollPane = new JScrollPane(scrollableList);
 		scrollPane.setViewportBorder(null);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		add(scrollPane, "cell 0 1,width 150,height 250");
+		add(scrollPane, "cell 0 1,grow");
 		
 	}
 
--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/workflowview/WorkFlowView.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/workflowview/WorkFlowView.java
@@ -8,6 +8,14 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import edu.wpi.cs.wpisuitetng.modules.taskmanager.view.taskstatus.TaskStatusView;
+import net.miginfocom.swing.MigLayout;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.SpringLayout;
 
 @SuppressWarnings("serial")
 public class WorkFlowView extends JPanel {
@@ -33,15 +41,15 @@ public class WorkFlowView extends JPanel {
 		
 		taskStatusPanel = new JPanel();
 		this.add(taskStatusPanel, BorderLayout.CENTER);
-		taskStatusPanel.setLayout(new GridLayout(1, 4, 0, 0));
 		TaskStatusView taskStatusNew = new TaskStatusView("New");
 		TaskStatusView taskStatusSelDev = new TaskStatusView("Selected for Development");
 		TaskStatusView taskStatusInDev = new TaskStatusView("Currently in Development");
 		TaskStatusView taskStatusDone = new TaskStatusView("Completed");
-		taskStatusPanel.add(taskStatusNew);
-		taskStatusPanel.add(taskStatusSelDev);
-		taskStatusPanel.add(taskStatusInDev);
-		taskStatusPanel.add(taskStatusDone);
+		taskStatusPanel.setLayout(new MigLayout("", "[250px][250px][250px][250px]", "[278px,grow 500]"));
+		taskStatusPanel.add(taskStatusNew, "cell 0 0,grow");
+		taskStatusPanel.add(taskStatusSelDev, "cell 1 0,grow");
+		taskStatusPanel.add(taskStatusInDev, "cell 2 0,grow");
+		taskStatusPanel.add(taskStatusDone, "cell 3 0,grow");
 		
 	}
 
