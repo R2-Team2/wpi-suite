c6d793a77f58038506c7d0d399b65eceb1ab8a24
Selim Tanriverdi <setanriverdi@wpi.edu>
2014-11-14 17:19:27 -0500

    Add Vertical Scrollbar to Task Status WPI-25

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/taskstatus/TaskStatusView.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/taskstatus/TaskStatusView.java
@@ -1,35 +1,20 @@
 package edu.wpi.cs.wpisuitetng.modules.taskmanager.view.taskstatus;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JList;
 import javax.swing.JPanel;
-
-import java.awt.BorderLayout;
-
+import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
-
-import java.awt.GridLayout;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.EtchedBorder;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
+import javax.swing.text.StyledDocument;
 
 import net.miginfocom.swing.MigLayout;
-
-import java.awt.GridBagLayout;
-
-import javax.swing.JScrollBar;
-import javax.swing.JList;
-import javax.swing.JScrollPane;
-import javax.swing.border.MatteBorder;
-
-import java.awt.Color;
-
-import javax.swing.ScrollPaneConstants;
-
-import java.awt.Font;
-import java.util.ArrayList;
-
-import javax.swing.JButton;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 
 public class TaskStatusView extends JPanel {
 
@@ -42,18 +27,28 @@ public class TaskStatusView extends JPanel {
 		this.taskStatusObj = new TaskStatus(title);
 		setLayout(new MigLayout("", "[236px]", "[26px][200px,grow 500]"));
 		
-		JTextPane txtpnTitle = new JTextPane();
-		txtpnTitle.setForeground(new Color(0, 0, 0));
-		txtpnTitle.setEditable(false);
-		txtpnTitle.setFont(txtpnTitle.getFont().deriveFont(txtpnTitle.getFont().getSize() + 5f));
-		txtpnTitle.setText(this.taskStatusObj.getName());
-		add(txtpnTitle, "cell 0 0,alignx left,aligny top");
-
 		JList scrollableList = new JList(this.taskStatusObj);
 		JScrollPane scrollPane = new JScrollPane(scrollableList);
 		scrollPane.setViewportBorder(null);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		add(scrollPane, "cell 0 1,grow");
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setBorder(new EtchedBorder());
+		this.add(scrollPane, "cell 0 1,grow");
+		
+		
+		StyledDocument document = new DefaultStyledDocument();
+		javax.swing.text.Style defaultStyle = document.getStyle(StyleContext.DEFAULT_STYLE);
+		StyleConstants.setAlignment(defaultStyle, StyleConstants.ALIGN_CENTER);
+		
+		JTextPane txtpnTitle = new JTextPane(document);
+		txtpnTitle.setBorder(new EtchedBorder());
+		txtpnTitle.setForeground(new Color(0, 0, 0));
+		txtpnTitle.setEditable(false);
+		txtpnTitle.setFont(txtpnTitle.getFont().deriveFont(txtpnTitle.getFont().getSize() + 10f));
+		txtpnTitle.setText(this.taskStatusObj.getName());
+		txtpnTitle.setPreferredSize(new Dimension(250,200));
+		//txtpnTitle.setAlignmentX(CENTER_ALIGNMENT);
+		this.add(txtpnTitle, "cell 0 0,alignx center,aligny top");
 		
 	}
 
