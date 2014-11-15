08400ab6f6eff48d32e711998ea1402c5be1d7bd
Selim Tanriverdi <setanriverdi@wpi.edu>
2014-11-14 23:23:36 -0500

    Apply Min and Max size to TaskStatusView in Workflow View

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/taskstatus/TaskStatusView.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/taskstatus/TaskStatusView.java
@@ -1,8 +1,6 @@
 package edu.wpi.cs.wpisuitetng.modules.taskmanager.view.taskstatus;
 
 import java.awt.Color;
-import java.awt.Dimension;
-
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -16,6 +14,9 @@ import javax.swing.text.StyledDocument;
 
 import net.miginfocom.swing.MigLayout;
 
+import javax.swing.UIManager;
+
+@SuppressWarnings("serial")
 public class TaskStatusView extends JPanel {
 
 	TaskStatus taskStatusObj;
@@ -25,12 +26,13 @@ public class TaskStatusView extends JPanel {
 	 */
 	public TaskStatusView(String title) {
 		this.taskStatusObj = new TaskStatus(title);
-		setLayout(new MigLayout("", "[236px]", "[26px][200px,grow 500]"));
+		setLayout(new MigLayout("", "[236px,grow]", "[26px][200px,grow 500]"));
 		
 		JList scrollableList = new JList(this.taskStatusObj);
 		JScrollPane scrollPane = new JScrollPane(scrollableList);
 		scrollPane.setViewportBorder(null);
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		//Change Vertical Scroll Bar Policy to AS_NEEDED When Task Cards are developed
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		scrollPane.setBorder(new EtchedBorder());
 		this.add(scrollPane, "cell 0 1,grow");
@@ -41,14 +43,13 @@ public class TaskStatusView extends JPanel {
 		StyleConstants.setAlignment(defaultStyle, StyleConstants.ALIGN_CENTER);
 		
 		JTextPane txtpnTitle = new JTextPane(document);
-		txtpnTitle.setBorder(new EtchedBorder());
+		txtpnTitle.setBackground(UIManager.getColor("Button.background"));
+		txtpnTitle.setBorder(null);
 		txtpnTitle.setForeground(new Color(0, 0, 0));
 		txtpnTitle.setEditable(false);
-		txtpnTitle.setFont(txtpnTitle.getFont().deriveFont(txtpnTitle.getFont().getSize() + 10f));
-		txtpnTitle.setText(this.taskStatusObj.getName());
-		txtpnTitle.setPreferredSize(new Dimension(250,200));
-		//txtpnTitle.setAlignmentX(CENTER_ALIGNMENT);
-		this.add(txtpnTitle, "cell 0 0,alignx center,aligny top");
+		txtpnTitle.setFont(txtpnTitle.getFont().deriveFont(20f));
+		txtpnTitle.setText(this.taskStatusObj.getName());		
+		this.add(txtpnTitle, "cell 0 0,alignx center,aligny center");
 		
 	}
 
--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/workflowview/WorkFlowView.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/workflowview/WorkFlowView.java
@@ -9,9 +9,14 @@ import javax.swing.JTextField;
 
 import edu.wpi.cs.wpisuitetng.modules.taskmanager.view.taskstatus.TaskStatusView;
 import net.miginfocom.swing.MigLayout;
+
 import java.awt.FlowLayout;
+
 import javax.swing.BoxLayout;
+import javax.swing.JScrollBar;
 import javax.swing.SpringLayout;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
 
 @SuppressWarnings("serial")
 public class WorkFlowView extends JPanel {
@@ -24,13 +29,20 @@ public class WorkFlowView extends JPanel {
 	public WorkFlowView() {
 		
 		setLayout(new BorderLayout());
+		
+		JScrollBar hbar = new JScrollBar(JScrollBar.HORIZONTAL,30,20,0,300);
+		this.add(hbar, BorderLayout.SOUTH);
+		//JScrollPane scrollPane = new JScrollPane();
+		//this.add(scrollPane, BorderLayout.SOUTH);
+		
 		taskStatusPanel = new JPanel();
 		this.add(taskStatusPanel, BorderLayout.CENTER);
-		TaskStatusView taskStatusNew = new TaskStatusView("New");
+		TaskStatusView taskStatusNew = new TaskStatusView("TITLE THAT IS SUPER UBER LONG OMG THIS IS REALLY LONG AAAAAAA AAAAA AAAAA  AAA AAAAA");
 		TaskStatusView taskStatusSelDev = new TaskStatusView("Selected for Development");
 		TaskStatusView taskStatusInDev = new TaskStatusView("Currently in Development");
 		TaskStatusView taskStatusDone = new TaskStatusView("Completed");
-		taskStatusPanel.setLayout(new MigLayout("", "[250px][250px][250px][250px]", "[278px,grow 500]"));
+		
+		taskStatusPanel.setLayout(new MigLayout("", "[350px:n:500px,grow,left][350px:n:500px,grow,left][350px:n:500px,grow,left][350px:n:500px,grow,left]", "[278px,grow 500]"));
 		taskStatusPanel.add(taskStatusNew, "cell 0 0,grow");
 		taskStatusPanel.add(taskStatusSelDev, "cell 1 0,grow");
 		taskStatusPanel.add(taskStatusInDev, "cell 2 0,grow");
