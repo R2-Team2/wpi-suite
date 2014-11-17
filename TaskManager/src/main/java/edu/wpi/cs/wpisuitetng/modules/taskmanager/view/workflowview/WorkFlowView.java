8c0b11924a67493b8a49ee56d9fe9132e2fdb807
dcfox <dcfox@wpi.edu>
2014-11-16 22:23:29 -0500

    Update WorkFlowView.java

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/workflowview/WorkFlowView.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/workflowview/WorkFlowView.java
@@ -1,6 +1,11 @@
-
-
-
+  * All rights reserved. This program and the accompanying materials
+  * are made available under the terms of the Eclipse Public License v1.0
+  * which accompanies this distribution, and is available at
+  * http://www.eclipse.org/legal/epl-v10.html
+  *
+  * Contributors:
+  *    R2-Team2
+  ******************************************************************************/
 //this class has depricated through task 24- create collapsing sidebar.
 package edu.wpi.cs.wpisuitetng.modules.taskmanager.view.workflowview;
 
