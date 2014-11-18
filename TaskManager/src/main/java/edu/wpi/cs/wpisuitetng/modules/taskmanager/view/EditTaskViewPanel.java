0eb6b7d10428a57e2bfb3fe5232951d45dde151f
pchambers <peter.chambers1@gmail.com>
2014-11-17 21:26:36 -0500

    Add empty clas for EditStatusView

---
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/EditTaskViewPanel.java
@@ -0,0 +1,17 @@
+package edu.wpi.cs.wpisuitetng.modules.taskmanager.view;
+
+import javax.swing.JPanel;
+
+import edu.wpi.cs.wpisuitetng.modules.taskmanager.models.Task;
+
+public class EditTaskViewPanel extends JPanel{
+	
+	Task givenTask;
+	
+	public EditTaskViewPanel()
+	{		
+		
+	}
+	
+	
+}
