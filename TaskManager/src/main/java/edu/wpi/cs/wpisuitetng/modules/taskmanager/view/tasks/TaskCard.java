05e48c17e4107fa04751e76b323f53930f392a5b
Dmytro <dbogatov@wpi.edu>
2014-11-18 22:52:39 -0500

    Add long title truncation and tool tip for task title.

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/tasks/TaskCard.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/view/tasks/TaskCard.java
@@ -1,47 +1,21 @@
 /*******************************************************************************
- * Copyright (c) 2013 WPI-Suite
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * Contributors:
- * 	Team R2-Team2
+ * Copyright (c) 2013 WPI-Suite All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which accompanies this
+ * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html Contributors: Team
+ * R2-Team2
  ******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.taskmanager.view.tasks;
 
-import javax.swing.JPanel;
-import javax.swing.BoxLayout;
-import javax.swing.JLabel;
-
-import java.awt.GridBagLayout;
-
-import javax.swing.border.LineBorder;
-import javax.swing.text.MutableAttributeSet;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledDocument;
-
 import java.awt.Color;
-import java.awt.GridBagConstraints;
 import java.awt.Font;
-import java.awt.Insets;
 
-import javax.swing.JTextField;
-import javax.swing.JList;
-import javax.swing.AbstractListModel;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextPane;
+import javax.swing.UIManager;
+import javax.swing.border.LineBorder;
 
 import net.miginfocom.swing.MigLayout;
-
-import java.awt.GridLayout;
-
-import javax.swing.JSeparator;
-import javax.swing.SwingConstants;
-import javax.swing.JTextPane;
-import javax.swing.JSplitPane;
-import javax.swing.JTextArea;
-import javax.swing.UIManager;
-import javax.swing.border.BevelBorder;
-
 import edu.wpi.cs.wpisuitetng.modules.taskmanager.models.Task;
 
 /**
@@ -49,44 +23,47 @@ import edu.wpi.cs.wpisuitetng.modules.taskmanager.models.Task;
  */
 public class TaskCard extends JPanel {
 
-	private Task taskObj;
-	JTextPane taskName = new JTextPane();
-	
-	/**
-	 * Create the panel.
-	 */
-	public TaskCard(String nameData, String dateData, String userNameData) {
-		setBorder(new LineBorder(Color.black));
-		setLayout(new MigLayout("", "[grow,fill]", "[grow][bottom]"));
-		
-		
-		taskName.setText(nameData);
-		taskName.setFont(new Font("Tahoma", Font.BOLD, 14));
-		taskName.setBackground(UIManager.getColor("Button.background"));
-		add(taskName, "cell 0 0,alignx center,aligny center");
-		
-		JPanel infoPanel = new JPanel();
-		add(infoPanel, "cell 0 1,grow");
-		infoPanel.setLayout(new MigLayout("", "[grow][grow]", "[grow]"));
-		
-		JLabel date = new JLabel(dateData);
-		date.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		infoPanel.add(date, "cell 0 0,alignx left");
-		
-		JLabel userName = new JLabel(userNameData);
-		userName.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		infoPanel.add(userName, "cell 1 0,alignx right");
-	}
-//	
-//	public void setTaskCardName(String name){
-//		taskName.setText(name);
-//	}
+    private Task taskObj;
+    JTextPane taskName = new JTextPane();
 
-	public Task getTaskObj() {
-		return taskObj;
-	}
+    /**
+     * Create the panel.
+     */
+    public TaskCard(String nameData, String dateData, String userNameData) {
+        this.setBorder(new LineBorder(Color.black));
+        this.setLayout(new MigLayout("", "[grow,fill]", "[grow][bottom]"));
 
-	public void setTaskObj(Task taskObj) {
-		this.taskObj = taskObj;
-	}
+        // truncates the displayed task title if it's longer than 25 characters.
+        this.taskName.setToolTipText(nameData);
+        nameData = nameData.length() > 25 ? nameData.substring(0, 25).concat("...") : nameData;
+        this.taskName.setText(nameData);
+        this.taskName.setFont(new Font("Tahoma", Font.BOLD, 14));
+        this.taskName.setBackground(UIManager.getColor("Button.background"));
+        this.add(this.taskName, "cell 0 0,alignx center,aligny center");
+
+        JPanel infoPanel = new JPanel();
+        this.add(infoPanel, "cell 0 1,grow");
+        infoPanel.setLayout(new MigLayout("", "[grow][grow]", "[grow]"));
+
+        JLabel date = new JLabel(dateData);
+        date.setFont(new Font("Tahoma", Font.PLAIN, 12));
+        infoPanel.add(date, "cell 0 0,alignx left");
+
+        JLabel userName = new JLabel(userNameData);
+        userName.setFont(new Font("Tahoma", Font.PLAIN, 12));
+        infoPanel.add(userName, "cell 1 0,alignx right");
+    }
+
+    //
+    // public void setTaskCardName(String name){
+    // taskName.setText(name);
+    // }
+
+    public Task getTaskObj() {
+        return this.taskObj;
+    }
+
+    public void setTaskObj(Task taskObj) {
+        this.taskObj = taskObj;
+    }
 }
