08b77b46593a4f0247e62dad32ce6af7e8b9050a
npmanor <npmanor@github.com>
2014-12-08 12:36:23 -0500

    Fix errors.

--- TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/actions/TaskStatusAction.java
+++ TaskManager/src/main/java/edu/wpi/cs/wpisuitetng/modules/taskmanager/actions/TaskStatusAction.java
@@ -2,19 +2,19 @@ package edu.wpi.cs.wpisuitetng.modules.taskmanager.actions;
 
 public class TaskStatusAction extends AbstractAction {
 
-    private Enum<TaskStatusAction.Actions> action;
+	private Enum<TaskStatusAction.Actions> action;
 
-    public static enum Actions {
+	public static enum Actions {
 
-    }
+	}
 
-    public TaskStatusAction(Enum<TaskStatusAction.Actions> action) {
-        this.action = action;
-    }
+	public TaskStatusAction(Enum<TaskStatusAction.Actions> action) {
+		this.action = action;
+	}
 
-    @Override
-    public num<TaskStatusAction.Actions> getAction() {
-        return action;
-    }
+	@Override
+	public Enum<TaskStatusAction.Actions> getAction() {
+		return action;
+	}
 
 }
